292e41d2d8df44a76dd4918c778550c74673663e
johnrenko <vishnou@gmail.com>
2016-11-27 19:42:28 +0100

    remove auth0

--- App/Containers/LogIn.js
+++ App/Containers/LogIn.js
@@ -11,8 +11,6 @@ import Icon from 'react-native-vector-icons/FontAwesome'
 import Animatable from 'react-native-animatable'
 import { SocialIcon } from 'react-native-elements'
 import { Actions as NavigationActions } from 'react-native-router-flux'
-import Auth0Lock from 'react-native-lock'
-import Auth0 from 'auth0-react-native'
 import * as firebase from 'firebase'
 import { Button } from 'react-native-elements'
 
@@ -35,52 +33,6 @@ const database = firebase.database();
 
 class LogIn extends React.Component {
   componentDidMount() {
-    const lock = new Auth0Lock({
-      clientId: 'LShnQxjBKHw5zi7dOjs56lEuIaN0J7hT', 
-      domain: 'johnrenko.eu.auth0.com'
-    });
-    lock.show({}, (err, profile, token) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      const options = {
-          id_token : token.idToken,
-          api : 'firebase',
-          scope : 'openid name email displayName',
-          target: 'LShnQxjBKHw5zi7dOjs56lEuIaN0J7hT'
-      };
-        const auth0 = new Auth0({
-          domain:       'johnrenko.eu.auth0.com',
-          clientID:     'JgHeQwYZvgF5yoeMNQ8KWgg23DUzCTRI',
-          callbackURL:  'http://google.fr',
-          responseType: 'token'
-        });
-      auth0.getDelegationToken(options, function(err, result) {
-
-        if(!err) {
-          // Exchange the delegate token for a Firebase auth token
-          firebase.auth().signInWithCustomToken(result.id_token).catch(function(error) {
-            console.log(error);
-          });
-        }
-      });
-      firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-          const uid = user.uid;
-          const firebase_user_ref = database.ref('users/');
-          firebase_user_ref.push({
-            "text": uid
-          });
-
-      } else {
-          const firebase_user_ref = database.ref('users/');
-          firebase_user_ref.push({
-            "text": "test"
-          });
-        }
-      });
-    });
   }
 
   onPress() {
